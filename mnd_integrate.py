58052ca2131767aa355f9d1ae46b2558aed38268
Karthikeya <karthikeya@MyMachine.(none)>
2015-01-07 18:16:23 +0100

    msg

--- mnd_integrate.py
+++ mnd_integrate.py
@@ -11,12 +11,8 @@ sigma = matrix([[0.1, 0, 0, 0],
 # mean vector
 mu = array([0,0,0,0])
 
-# input
-#x = array([2.1,3.5,8, 9.5])
-
 def norm_pdf_multivariate(x, mu, sigma):
 
-        #norm_const = 1.0/ ( math.pow((2*pi),float(size)/2) * math.pow(det,1.0/2) )
         x_mu = matrix(x - mu)
         inv = sigma.I        
         result = exp(-0.5 * (x_mu * inv * x_mu.T))
@@ -31,6 +27,7 @@ norm_const =  math.pow((2*pi),float(len(mu))/2) * math.pow(det,1.0/2)
 
 print "norm_const: ",norm_const
 
+#Proper selection of the xl and xu is very important to get the right result
 result, error = mcquad(lambda x: norm_pdf_multivariate(x, mu, sigma), xl=[-1.], xu=[1.], npoints=100000)
 
 print "MCMC Integration result: ",result
