bdd111a22010ba20d7a1dd7880377c3e4217f142
Karthikeya <karthikeya@MyMachine.(none)>
2015-01-15 12:04:57 +0100

    Z

---
+++ Utils/factor_graph.py
@@ -0,0 +1,36 @@
+import itertools
+'''
+This class allows to create a factor graph and stores it. It also allows to coarsen the factor graph by deleting listed factors.
+
+Class Variables:  
+dim - Dimension of the dataset (Number of nodes in the graph)
+nodes - list of nodes (in integer format)
+factors - A dictionary with <key> indicating the number of interacting nodes and each <value> is a <list> of <set> objects indicating the factors
+
+Class Methods:
+__init__(dim) - Constructor with dimension as the parameter
+create_factor_graph(max_factors) - Create the factor graph based on the specified <max_factors> 
+coarsen_factor_graph(delete_list) - Coarsens the factor graph by deleting listed factors passed as parameter
+'''
+class factor_graph:
+	def __init__(self, dim):
+		self.dim = dim
+		self.nodes = []
+		for n in xrange(1,dim+1):
+                        self.nodes[len(self.nodes):] = [n]
+		
+		self.factors = {}
+
+        def create_factor_graph(self, max_factors):
+		if self.dim < max_factors:
+			print "<max_factors> cannot be greater than number of nodes in the graph"
+			exit(0)
+		for f in xrange(1,max_factors+1):
+			combination = itertools.combinations(self.nodes, f)
+			self.factors[f] = list(combination)
+
+	def coarsen_factor_graph(self, delete_list):
+		for i in delete_list:
+			self.factors[len(i)].remove(i)
+		
+	
---
+++ Utils/test_factor_graph.py
@@ -0,0 +1,11 @@
+from factor_graph import *
+
+fac = factor_graph(4)
+fac.create_factor_graph(3)
+
+print "Node List: ",fac.nodes
+print "Complete factors: ",fac.factors
+fac.coarsen_factor_graph([(1, 2, 4), (1, 3, 4), (2, 3, 4)])
+
+print "Factors after coarsening: ",fac.factors
+
